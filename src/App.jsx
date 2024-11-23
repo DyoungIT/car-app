4c8680f382bd2303fb27bbf0023de6bf62053130
Denis <denis.gubenko777@gmail.com>
2024-11-23 21:43:56 +0200

    header-create

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import style from "./App.module.css";
+import { Header } from "./Components/Header/Header";
+import { Catalog } from "./Components/Navbar/Catalog/Catalog";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className={style.mainContainer}>
+        <Header />
+        <Routes>
+          {/* <Route path="/catalog" element={<Catalog/>} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import style from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <nav className={style.container}>
+      <div>
+        <Link to="/"><img src="logo.svg" width={50} height={50}/></Link>
+      </div>
+      <div>
+        <Link to="/catalog">Каталог</Link>
+      </div>
+      <div>
+        <Link to="/about">О компании</Link>
+      </div>
+      <div>
+        <Link to="/reviews">Отзывы</Link>
+      </div>
+    </nav>
+  );
+}
