34afd5929060d31d8e452a8ceb6c4af0bfc1a192
Denis <denis.gubenko777@gmail.com>
2024-11-28 23:43:46 +0200

    car-page-catalog

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Header } from "./Components/Header/Header";
 import { Catalog } from "./Components/Navbar/Catalog/Catalog";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <BrowserRouter>
       <div className={style.mainContainer}>
         <Header />
         <Routes>
-          {/* <Route path="/catalog" element={<Catalog/>} /> */}
+           <Route path="/catalog" element={<Catalog/>} /> 
         </Routes>
       </div>
     </BrowserRouter>
